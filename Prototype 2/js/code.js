dec08e1b9aff606fab726eba5ca268bb21576e5f
Jeffrey <Jeffogya@gmail.com>
2013-11-21 22:11:33 +0100

    styled the prototype

--- Prototype 2/js/code.js
+++ Prototype 2/js/code.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
 		var passCode =$('#pass').val();
 		if(passCode=="passcode")
 		{
-			$('#dis').slideDown().html('<span>Correct</span>');
 			window.location.replace("page_2.html");
 			return false;
 		} else {
-			$('#dis').slideDown().html('<span>Please type a correct password</span>');
+			$('#dis').slideDown().html('<span class="red">Please type a correct password</span>');
 			return false;
 		}
 	});
